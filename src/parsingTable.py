d4ff28c8ed8079f601dcfd5b4fabc4a5e63e103a
mohamedpsc <mohamedpsc@gmail.com>
2017-05-01 22:42:08 +0200

    added parsing table

---
+++ src/parsingTable.py
@@ -0,0 +1,15 @@
+class Table(object):
+    def __init__(self, terminals, non_terminals, follows, starts):
+        self.table = dict()
+        for non_terminal in non_terminals:
+            for terminal in terminals:
+                if terminal in starts[non_terminal]:
+                    # TODO replace the None with the required production
+                    self.table[(non_terminal, terminal)] = None
+                elif terminal in follows[non_terminal]:
+                    # TODO replace the None with the required production
+                    self.table[(non_terminal, terminal)] = None
+                else:
+                    self.table[(non_terminal, terminal)] = None
+
+    def trace(self):
