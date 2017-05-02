4e333eb3927462eb595b7e3ddf8f4953f1e8fa57
mohamedpsc <mohamedpsc@gmail.com>
2017-05-02 15:17:51 +0200

    Table created

--- src/parsingTable.py
+++ src/parsingTable.py
@@ -1,16 +1,15 @@
 class Table(object):
-    def __init__(self, terminals, non_terminals, follows, starts):
+    def __init__(self, cfg, firsts, follows):
         self.table = dict()
-        for non_terminal in non_terminals:
-            for terminal in terminals:
-                if terminal in starts[non_terminal]:
-                    # TODO replace the None with the required production
-                    self.table[(non_terminal, terminal)] = None
+        for non_terminal in cfg.non_terminals:
+            for terminal in cfg.terminals:
+                if terminal in firsts.get(non_terminal):
+                    self.table[(non_terminal, terminal)] = cfg.getProduction(non_terminal, terminal)
                 elif terminal in follows[non_terminal]:
-                    # TODO replace the None with the required production
-                    self.table[(non_terminal, terminal)] = None
+                    self.table[(non_terminal, terminal)] = "sync"
                 else:
                     self.table[(non_terminal, terminal)] = None
+        print(self.table)
 
     def trace(self):
         # TODO Implement the trace function
