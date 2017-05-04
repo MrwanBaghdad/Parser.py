6234f8db131613b181a7e781c749815947068aa7
mohamedpsc <mohamedpsc@gmail.com>
2017-05-04 15:14:50 +0200

    fixed string replacement Function

--- src/CFGReader.py
+++ src/CFGReader.py
@@ -7,17 +7,27 @@ class Reader(object):
         self.non_terminals = []
         self.file_lines = []
         self.productions = {}
+        self.productions2 = {}
         f = open(file_name, 'r')
         self.data = f.readlines()
         f.close()
         self.prepareData()
+        # print(self.productions)
+        # for k in self.non_terminals:
+        #     print(k + " => " + self.productions[k])
         self.eliminateLeftRecusion()
+        for non_terminal in self.non_terminals:
+            array = self.productions[non_terminal].split("|")
+            temp = []
+            for element in array:
+                temp.append(element.strip())
+            self.productions2[non_terminal] = temp
         # print(self.file_lines)
         # print(self.terminals)
         # print(self.non_terminals)
-        # print(self.productions)
-        # for k in self.non_terminals:
-        #     print(k+" => "+self.productions[k])
+        print(self.productions)
+        for k in self.non_terminals:
+            print(k + " => " + self.productions[k])
 
     def prepareData(self):
         # Read Productions From the given file and analyze data into terminals and non-terminals
@@ -50,14 +60,16 @@ class Reader(object):
                     s = self.productions[self.non_terminals[i]].find(self.non_terminals[j], s)
                     if s == -1:
                         break
-                    if s+len(self.non_terminals[j]) < len(self.productions[self.non_terminals[i]]):
+                    if s + len(self.non_terminals[j]) < len(self.productions[self.non_terminals[i]]):
                         if s == 0:
-                            if self.productions[self.non_terminals[i]][s+len(self.non_terminals[j])] == ' ':
+                            if self.productions[self.non_terminals[i]][s + len(self.non_terminals[j])] == ' ':
                                 self.replaceString(self.non_terminals[i], self.non_terminals[j], s)
                             else:
                                 s += 1
                         else:
-                            if self.productions[self.non_terminals[i]][s-1] == ' ' and self.productions[self.non_terminals[i]][s+len(self.non_terminals[j])] == ' ':
+                            if self.productions[self.non_terminals[i]][s - 1] == ' ' and \
+                                            self.productions[self.non_terminals[i]][
+                                                        s + len(self.non_terminals[j])] == ' ':
                                 self.replaceString(self.non_terminals[i], self.non_terminals[j], s)
                             else:
                                 s += 1
@@ -65,19 +77,44 @@ class Reader(object):
 
     def replaceString(self, key, value, s):
         # Replace a non-terminal with it's corresponding value in the production dictionary
-        trace = s + len(value)
+        end = s + len(value)
+        beginning = s
+        while beginning > 0:
+            if self.productions[key][beginning] == '|':
+                break;
+            beginning -= 1
+        previous = self.productions[key][beginning:s]
         following = ""
-        while trace < len(self.productions[key]):
-            if self.productions[key][trace] == '|':
+        while end < len(self.productions[key]):
+            if self.productions[key][end] == '|':
                 break
-            following += self.productions[key][trace]
-            trace += 1
-        new_string = ""
-        splitted_productions = self.productions[value].split('|')
-        for production in splitted_productions:
-            new_string += production.strip() + " " + following.strip() + " | "
-        new_string = new_string[:len(new_string) - 3]
-        self.productions[key] = self.productions[key].replace((value+following).strip(), new_string, 1)
+            following += self.productions[key][end]
+            end += 1
+        if previous in ['| ', '|'] and following in [' |', '|']:
+            self.productions[key] = self.productions[key].replace(value.strip(), self.productions[value], 1)
+        elif previous in ['| ', '|']:
+            new_string = ""
+            splitted_productions = self.productions[value].split('|')
+            for production in splitted_productions:
+                new_string += production.strip() + " " + following.strip() + " | "
+            new_string = new_string[:len(new_string) - 3]
+            self.productions[key] = self.productions[key].replace((value + following).strip(), new_string, 1)
+        elif following in [' |', '|']:
+            new_string = ""
+            splitted_productions = self.productions[value].split('|')
+            for production in splitted_productions:
+                new_string += previous.strip() + " " + production.strip() + " | "
+            new_string = new_string[:len(new_string) - 3]
+            self.productions[key] = self.productions[key].replace((previous + value).strip(), new_string, 1)
+        else:
+            new_string = ""
+            splitted_productions = self.productions[value].split('|')
+            for production in splitted_productions:
+                new_string += previous.strip() + " " + production.strip() + " " + following.strip() + " | "
+            new_string = new_string[:len(new_string) - 3]
+            self.productions[key] = self.productions[key].replace((previous + value + following).strip(), new_string,
+                                                                  1).replace("| |", "|")
+        print(self.productions[key])
 
     def eliminateImmediateLeftRecusion(self, key):
         # This function eliminate immediate Left Recursion => A = AB|Aa|A
@@ -97,7 +134,7 @@ class Reader(object):
             string = ""
             for non in non_recursive:
                 string += non.strip() + " " + new_non_terminal + " | "
-            string = string[:len(string)-3]
+            string = string[:len(string) - 3]
             self.productions[key] = string
             new_non_terminal_value = ""
             for production in recursive:
@@ -105,3 +142,22 @@ class Reader(object):
             new_non_terminal_value += "None"
             self.non_terminals.append(new_non_terminal)
             self.productions[new_non_terminal] = new_non_terminal_value
+
+    def getProduction(self, non_terminal, terminal):
+        for production in self.productions2[non_terminal]:
+            if production.find(terminal) == 0:
+                return production
+        return None
+
+    def nonTerminaleName(self, non_terminal):
+        new_name = non_terminal + "`"
+        while new_name in self.non_terminals:
+            new_name += "`"
+        return new_name
+
+    def leftFactoring(self, key):
+        definitions = [production.strip() for production in self.productions[key].split("|")]
+        length = len(definitions[0])
+        for definition in definitions:
+            if len(definition) < length:
+                length = len(definition)
