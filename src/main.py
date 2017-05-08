c609d9c2a08b160f3b2e32d40ee9eddbafdf83d4
Marwan <mrwanbaghdad76@gmail.com>
2017-05-08 22:07:34 +0200

    remove wildcards import

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
-from CFGReader import Reader
-reader = Reader()
+# from CFGReader import Reader
+# from parsingTable import *
+# from construct_first_follows import *
+import CFGReader
+import parsingTable
+import construct_first_follows
+
+parsingTable = parsingTable.Table(construct_first_follows.reader, construct_first_follows.firsts, construct_first_follows.follows)
+# parsingTable = Table(reader, firsts, follows)
+print("done")
