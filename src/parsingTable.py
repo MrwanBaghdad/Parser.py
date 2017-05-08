d98992b09d7fc3392cb024f634877606a170c292
mohamedpsc <mohamedpsc@gmail.com>
2017-05-08 18:07:42 +0200

    Create Parsing table fixed

--- src/parsingTable.py
+++ src/parsingTable.py
@@ -3,19 +3,43 @@ logging.basicConfig(level=logging.DEBUG)
 class Table(object):
     def __init__(self, cfg, firsts, follows):
         self.cfg = cfg
-        self.temp_input = ['$',  '}', ';', '0', 'assign', 'id', '{', ')', 'num', 'relop', 'id', '(', 'if', ';', 'num', 'assign', 'id', ';', 'id', 'int']
+        self.temp_input = ['h', '+', 'h']
+        # self.temp_input = ['b', 'a', 'a']
+        # self.temp_input = ['}', ';', '0', 'assign', 'id', '{', ')', 'num', 'relop', 'id', '(', 'if', ';', 'num', 'assign', 'id', ';', 'id', 'int']
         self.table = dict()
         for non_terminal in cfg.non_terminals:
+            if '$' in follows[non_terminal]:
+                self.table[(non_terminal, '$')] = "None"
+            else:
+                self.table[(non_terminal, '$')] = None
             for terminal in cfg.terminals:
+                terminal = terminal.replace("'", '')
                 if terminal in firsts.get(non_terminal):
-                    self.table[(non_terminal, terminal)] = cfg.productions.get(non_terminal)
+                    productionsList = cfg.productions2.get(non_terminal)
+                    for production in productionsList:
+                        production = production.replace("'", "")
+                        s = production.find(' ')
+                        if s > 0:
+                            first = production[:s]
+                        else:
+                            first = production
+                        if first in cfg.non_terminals:
+                            if first == terminal or terminal in firsts.get(first):
+                                self.table[(non_terminal, terminal)] = production
+                                break
+                        elif "'"+first+"'" in cfg.terminals:
+                            if first == terminal:
+                                self.table[(non_terminal, terminal)] = production
+                                break
                 elif terminal in follows[non_terminal]:
-                    self.table[(non_terminal, terminal)] = "sync"
+                    if "None" in cfg.productions.get(non_terminal):
+                        self.table[(non_terminal, terminal)] = "None"
+                    else:
+                        self.table[(non_terminal, terminal)] = "sync"
                 else:
                     self.table[(non_terminal, terminal)] = None
-        # self.table[(cfg.non_terminals[0], '$')] = 'sync'
-        from tabulate import tabulate
-        print((self.table))
+        # from tabulate import tabulate
+        self.trace()
 
     def trace(self):
         stack = ['$', self.cfg.non_terminals[0]]
@@ -23,7 +47,8 @@ class Table(object):
         stack_peek = stack[-1]
         while stack_peek != '$':
             if terminal == '$':
-                print(stack_peek + " => " + data)
+                if self.table.get((stack_peek, terminal)) is not None:
+                    print(stack_peek + " => " + self.table.get((stack_peek, terminal)))
                 stack.pop()
             elif stack_peek in self.cfg.non_terminals:
                 data = self.table.get((stack_peek, terminal))
@@ -34,25 +59,31 @@ class Table(object):
                     print("Error: sync was found")
                     stack.pop()
                 else:
+                    stack.pop()
                     for element in reversed(data.split()):  # Done Pushing elements to stack and output the production
-                        if element.strip != "None":
+                        if element.strip() != "None":
                             stack.append(element.strip())
                     print(stack_peek+" => "+data)
-            elif stack_peek in self.cfg.terminals:
+            elif "'"+stack_peek+"'" in self.cfg.terminals:
                 if stack_peek != terminal:  # Done Matching
                     print("Error: missing " + stack_peek)
+                else:
+                    terminal = self.nextInput()
                 stack.pop()
-                terminal = self.nextInput()
+
             else:
-                print("Unknown Symbol => " + stack_peek)
+                print("Unknown Symbol => " + terminal)
             stack_peek = stack[-1]
+        print("accept")
 
     def nextInput(self):
         # temp = lexical_analyzer.next()
         # if temp is None:
         #     return '$';
         # return temp
-        return self.temp_input.pop()
+        if len(self.temp_input) > 0:
+            return self.temp_input.pop()
+        return '$'
 
 if __name__ == "__main__":
     import construct_first_follows as ff 
