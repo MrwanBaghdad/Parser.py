b8a3f2e0061391baf722399b418fc06961a8dcb4
mohamedpsc <mohamedpsc@gmail.com>
2017-05-12 13:45:48 +0200

    create new Branch

--- src/parsingTable.py
+++ src/parsingTable.py
@@ -3,12 +3,18 @@ logging.basicConfig(level=logging.DEBUG)
 class Table(object):
     def __init__(self, cfg, firsts, follows):
         self.cfg = cfg
-        self.temp_input = ['h', '+', 'h']
+        seen = set()
+        seen_add = seen.add
+        cfg.terminals = [x for x in cfg.terminals if not (x in seen or seen_add(x))]
+        seen = set()
+        seen_add = seen.add
+        cfg.non_terminals = [x for x in cfg.non_terminals if not (x in seen or seen_add(x))]
+        # self.temp_input = ['h', '+', 'h']
         # self.temp_input = ['b', 'a', 'a']
-        # self.temp_input = ['}', ';', '0', 'assign', 'id', '{', ')', 'num', 'relop', 'id', '(', 'if', ';', 'num', 'assign', 'id', ';', 'id', 'int']
+        self.temp_input = ['}', ';', '0', 'assign', 'id', '{', ')', 'num', 'relop', 'id', '(', 'if', ';', 'num', 'assign', 'id', ';', 'id', 'int']
         self.table = dict()
         for non_terminal in cfg.non_terminals:
-            if '$' in follows[non_terminal]:
+            if '$' in follows[non_terminal] and 'None' in firsts[non_terminal]:
                 self.table[(non_terminal, '$')] = "None"
             else:
                 self.table[(non_terminal, '$')] = None
@@ -27,7 +33,7 @@ class Table(object):
                             if first == terminal or terminal in firsts.get(first):
                                 self.table[(non_terminal, terminal)] = production
                                 break
-                        elif "'"+first+"'" in cfg.terminals:
+                        elif first in cfg.terminals:
                             if first == terminal:
                                 self.table[(non_terminal, terminal)] = production
                                 break
@@ -42,18 +48,22 @@ class Table(object):
         self.trace()
 
     def trace(self):
+        string = "" + self.cfg.non_terminals[0]
+        print(string)
         stack = ['$', self.cfg.non_terminals[0]]
         terminal = self.nextInput()
         stack_peek = stack[-1]
         while stack_peek != '$':
             if terminal == '$':
                 if self.table.get((stack_peek, terminal)) is not None:
-                    print(stack_peek + " => " + self.table.get((stack_peek, terminal)))
+                    string = self.replaceString(string, stack_peek, self.table.get((stack_peek, terminal)))
+                    print(string + " || " + stack_peek + " => " + self.table.get((stack_peek, terminal)))
                 stack.pop()
             elif stack_peek in self.cfg.non_terminals:
                 data = self.table.get((stack_peek, terminal))
                 if data is None:
-                    print("Error: (illegal " + stack_peek + ")")
+                    string = self.replaceString(string, stack_peek, '')
+                    print(string + " || " + "Error: (illegal " + stack_peek + ")")
                     terminal = self.nextInput()
                 elif data == 'sync':  # Done popping from the stack.
                     print("Error: sync was found")
@@ -63,16 +73,19 @@ class Table(object):
                     for element in reversed(data.split()):  # Done Pushing elements to stack and output the production
                         if element.strip() != "None":
                             stack.append(element.strip())
-                    print(stack_peek+" => "+data)
-            elif "'"+stack_peek+"'" in self.cfg.terminals:
+                    string = self.replaceString(string, stack_peek, data)
+                    print(string + " || " + stack_peek + " => " + data)
+            elif stack_peek in self.cfg.terminals:
                 if stack_peek != terminal:  # Done Matching
-                    print("Error: missing " + stack_peek)
+                    string = self.replaceString(string, stack_peek, '')
+                    print(string + " || " + "Error: missing " + stack_peek)
                 else:
                     terminal = self.nextInput()
                 stack.pop()
 
             else:
-                print("Unknown Symbol => " + terminal)
+                print(string + "Unknown Symbol => " + terminal)
+                terminal = self.nextInput()
             stack_peek = stack[-1]
         print("accept")
 
@@ -84,6 +97,17 @@ class Table(object):
         if len(self.temp_input) > 0:
             return self.temp_input.pop()
         return '$'
+
+    def replaceString(self, string, to_be_replaced, replacement):
+        if to_be_replaced == string:
+            return replacement
+        s = string.find(to_be_replaced, 0)
+        if s > 0:
+            if s + len(to_be_replaced) == len(string):
+                return string.replace(" " + to_be_replaced, " " + replacement)
+            else:
+                return string.replace(" " + to_be_replaced + " ", " " + replacement + " ")
+        return string.replace(to_be_replaced + " ", replacement + " ")
 
 if __name__ == "__main__":
     import construct_first_follows as ff 
